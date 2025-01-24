b94a3bbbfdb853d78024ac4bd904cf8307f2e0a5
milad <baranimilad70@yahoo.com>
2025-01-25 00:38:11 +0330

    start

---
+++ src/main/java/com/example/phonecontactmvn/PhoneContactMvnApplication.java
@@ -0,0 +1,13 @@
+package com.example.phonecontactmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneContactMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneContactMvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phonecontactmvn/PhoneContactMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phonecontactmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneContactMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
