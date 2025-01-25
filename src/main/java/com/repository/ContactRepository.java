b46093e7d9012d38c68925d0060d0b2abf53245e
milad <baranimiald14@gmail.com>
2025-01-25 08:43:57 +0330

    feat: update repository

--- src/main/java/com/repository/ContactRepository.java
+++ src/main/java/com/repository/ContactRepository.java
@@ -1,5 +1,12 @@
 package com.repository;
 
+import com.beans.Contact;
+import com.beans.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     List<Contact> findByUser(User user);
